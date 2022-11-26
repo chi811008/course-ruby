454c6606c1e0c5b68242f104e56a4da4285f958f
Seraphine_Tsui <chi811008@gmail.com>
2022-11-26 11:24:30 +0800

    feat: basic function work

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,5 +1,6 @@
 class UnitsController < ApplicationController
   before_action :set_unit, only: %i[ show edit update destroy ]
+  before_action :set_course
 
   # GET /units or /units.json
   def index
@@ -21,11 +22,10 @@ class UnitsController < ApplicationController
 
   # POST /units or /units.json
   def create
-    @unit = Unit.new(unit_params)
-
+    @unit = @course.unit.build(unit_params)
     respond_to do |format|
       if @unit.save
-        format.html { redirect_to unit_url(@unit), notice: "Unit was successfully created." }
+        format.html { redirect_to course_units_url, notice: "Unit was successfully created." }
         format.json { render :show, status: :created, location: @unit }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class UnitsController < ApplicationController
   def update
     respond_to do |format|
       if @unit.update(unit_params)
-        format.html { redirect_to unit_url(@unit), notice: "Unit was successfully updated." }
+        format.html { redirect_to course_units_url(@course, @unit), notice: "Unit was successfully updated." }
         format.json { render :show, status: :ok, location: @unit }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,12 +52,17 @@ class UnitsController < ApplicationController
     @unit.destroy
 
     respond_to do |format|
-      format.html { redirect_to units_url, notice: "Unit was successfully destroyed." }
+      format.html { redirect_to course_units_url, notice: "Unit was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_unit
       @unit = Unit.find(params[:id])
