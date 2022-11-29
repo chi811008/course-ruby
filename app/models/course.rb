5ab8c5d68c0d77ae1d437b6c4fc6231af7615175
Seraphine_Tsui <chi811008@gmail.com>
2022-11-29 11:13:48 +0800

    feat: add validations in course and unit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
 	has_many :unit
+	validates :name, presence: true
+	validates :lecturer_name, presence: true
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,6 @@
 class Unit < ApplicationRecord
 	belongs_to :course
+	validates :name, presence: true
+	validates :order, presence: true
+	validates :content, presence: true
 end
